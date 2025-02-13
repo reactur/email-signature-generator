649dd97660a4c2abdfd0bdc281e005d14e6457ea
yeshwanth1837 <yeshwanthnaidu62@gmail.com>
2025-02-13 16:44:48 +0530

    added loader

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
 import App from "./App";
 import SocialMediaIcons from "./SocialMediaIcons";
 import SvgToPngConverter from "./SvgtoPngConverter";
@@ -7,20 +8,45 @@ import HtmlToPngConverter from "./HtmlToPngConverter";
 import Navbar from "./components/Interface/NavBar";
 import Home from "./Home";
 import Footer from "./components/Interface/Footer";
+import Loader from "./components/Interface/Loader"; // Import your loader component
+
+function AppContent() {
+  const [loading, setLoading] = useState(false);
+  const location = useLocation();
+
+  useEffect(() => {
+    setLoading(true);
+    const timer = setTimeout(() => {
+      setLoading(false);
+      window.scrollTo(0,0)
+    }, 500); 
+
+    return () => clearTimeout(timer);
+  }, [location]);
+
+  return (
+    <>
+      <Navbar />
+      {loading && <Loader />} 
+      {!loading && (
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signature-generator" element={<App />} />
+          <Route path="/icons" element={<SocialMediaIcons />} />
+          <Route path="/svg-to-png" element={<SvgToPngConverter />} />
+          <Route path="/html-to-png" element={<HtmlToPngConverter />} />
+          <Route path="/cld" element={<ImageGallery />} />
+        </Routes>
+      )}
+      <Footer />
+    </>
+  );
+}
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signature-generator" element={<App />} />
-        <Route path="/icons" element={<SocialMediaIcons />} />
-        <Route path="/svg-to-png" element={<SvgToPngConverter />} />
-        <Route path="/html-to-png" element={<HtmlToPngConverter />} />
-        <Route path="/cld" element={<ImageGallery />} />
-      </Routes>
-      <Footer />
+      <AppContent />
     </BrowserRouter>
   );
 }
